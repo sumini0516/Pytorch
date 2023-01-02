9ed3425a71ece60d540a5af92d029a44620b2cbb
sumini0516 <sumini0516@catholic.ac.kr>
2023-01-02 19:29:44 +0900

    0102_1

---
+++ Masked word completion with BERT.py
@@ -0,0 +1,10 @@
+from transformers import BertTokenizer, BertModel
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertModel.from_pretrained('bert-based-undased')
+
+text = "Replace me by any text you'd like"
+encoded_input = tokenizer(text, return_tensors='pt')
+output = model(**encoded_input)
+
+print(output)
